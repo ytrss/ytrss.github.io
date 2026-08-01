aa2587516876fd2ba378b23e157292c194499a6c
ytrss <ytrss@users.noreply.github.com>
2026-08-01 16:08:05 +0200

    Added history graph

--- static/main.js
+++ static/main.js
@@ -15,7 +15,7 @@ async function get_history() {
 
 function set_title(history) {
     const status = history[history.length-1].status;
-    const title = document.querySelector("#title");
+    const title = document.querySelector(".title");
     if (status == 200) {
         title.innerHTML = "YouTube's RSS feed is <strong>online</strong>";
         title.classList.remove("offline");
@@ -27,40 +27,119 @@ function set_title(history) {
     }
 }
 
+function get_time_delta_string(start_date, end_date) {
+    start_date = new Date(start_date);
+    end_date = new Date(end_date);
+    let time_delta = end_date - start_date;
+    let unit = "millisecond";
+    if (time_delta < 60000) {
+        time_delta = Math.floor(time_delta / 1000);
+        unit = "second";
+    } else if (time_delta < 3600000) {
+        time_delta = Math.floor(time_delta / 60000);
+        unit = "minute";
+    } else if (time_delta < 86400000) {
+        time_delta = Math.floor(time_delta / 3600000);
+        unit = "hour";
+    } else {
+        time_delta = Math.floor(time_delta / 86400000);
+        unit = "day";
+    }
+    if (time_delta != 1) {
+        unit += "s";
+    }
+    return `${time_delta} ${unit}`;
+}
+
 function set_timer(history) {
-    const update_date = new Date(history[history.length-1].date);
-    const timer = document.querySelector("#timer");
+    const update_date = history[history.length-1].date;
+    const timer = document.querySelector(".timer");
     const update_timer = () => {
-        const current_date = new Date();
-        let time_delta = current_date - update_date;
-        let unit = "millisecond";
-        if (time_delta < 60000) {
-            time_delta = Math.floor(time_delta / 1000);
-            unit = "second";
-        } else if (time_delta < 3600000) {
-            time_delta = Math.floor(time_delta / 60000);
-            unit = "minute";
-        } else {
-            time_delta = Math.floor(time_delta / 3600000);
-            unit = "hour";
-        }
-        if (time_delta != 1) {
-            unit += "s";
-        }
-        timer.textContent = `Updated ${time_delta} ${unit} ago...`;
+        timer.textContent = `Updated ${get_time_delta_string(update_date, new Date())} ago...`;
     };
     update_timer();
-    return setInterval(update_timer, 500);
+    const interval = setInterval(update_timer, 500);
+    return () => clearInterval(interval);
+}
+
+function get_history_groups(history, groups) {
+    const small_group_size = Math.floor(history.length / groups);
+    const large_group_size = small_group_size + 1;
+    const large_groups = history.length - (small_group_size * groups);
+    const small_groups = groups - large_groups;
+    const group_sizes = [];
+    group_sizes.push(...Array(small_groups).fill(small_group_size));
+    group_sizes.push(...Array(large_groups).fill(large_group_size));
+    let start = 0;
+    const history_groups = [];
+    for (const group_size of group_sizes) {
+        const end = start + group_size;
+        history_groups.push(history.slice(start, end));
+        start = end;
+    }
+    return history_groups;
+}
+
+function set_graph(history) {
+    const graph = document.querySelector(".graph");
+    const status = graph.querySelector(".status");
+    const start_date = graph.querySelector(".date.start");
+    const end_date = graph.querySelector(".date.end");
+    const update_status = () => {
+        const groups = Math.floor(status.offsetWidth / 12);
+        const history_groups = get_history_groups(history, groups);
+        status.replaceChildren();
+        for (const history_group of history_groups) {
+            const group = document.createElement("div");
+            group.classList.add("group");
+            if (history_group.length > 0) {
+                const offline = history_group.some(entry => entry.status != 200);
+                if (offline) {
+                    group.classList.add("offline");
+                } else {
+                    group.classList.add("online");
+                }
+            }
+            status.appendChild(group);
+        }
+    };
+    const update_dates = () => {
+        if (history.length > 0) {
+            start_date.textContent = `${get_time_delta_string(history[0].date, new Date())} ago`;
+            end_date.textContent = `${get_time_delta_string(history[history.length-1].date, new Date())} ago`;
+        }
+    };
+    const observer = new ResizeObserver(update_status);
+    observer.observe(status);
+    update_dates();
+    const interval = setInterval(update_dates, 500);
+    return () => {
+        observer.disconnect();
+        clearInterval(interval);
+    };
+}
+
+function set_history_download(history) {
+    const download = document.querySelector(".download");
+    const json = JSON.stringify(history);
+    const blob = new Blob([json], { type: "application/json" });
+    download.href = URL.createObjectURL(blob);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    let timer_interval;
+    let clear_timer = () => {};
+    let clear_graph = () => {};
     const update_history = async () => {
-        clearInterval(timer_interval);
+        clear_timer();
+        clear_graph();
         const history = await get_history();
         set_title(history);
-        timer_interval = set_timer(history);
+        clear_timer = set_timer(history);
+        clear_graph = set_graph(history);
+        set_history_download(history);
+        console.info("Updated history");
     };
+    clear_graph = set_graph([]);
     update_history();
     setInterval(update_history, 300000);
 });
